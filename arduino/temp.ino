2d476002282031406375450f9134a832da6a7df7
Victor Limpens <68400813+Victqr@users.noreply.github.com>
2023-05-09 13:07:03 +0200

    Update temp.ino

--- arduino/temp.ino
+++ arduino/temp.ino
@@ -1,41 +1,41 @@
 #include <SoftwareSerial.h>
+SoftwareSerial bluetoothSerial(0, 1); // RX, TX pinnen voor de Bluetooth-module
+
+int motor = 1;
+int batterij = 2;
+int sensor3 = 3;
+int sensor4 = 4;
 
 
-float temperatuurmotor, temperatuurbaterij, temperatuursensor3, temperatuursensor4  ; 
-float UTempmotor, UTempbatterij, UTempsensor3, UTempsensor4; 
-int motor = 1;  
-int motortemper = 0; 
-int batterij = 2; 
-int baterijtemper = 0; 
-int sensor3 = 3;  
-int sensor3temper = 0; 
-int sensor4 = 4;  
-int sensor4temper = 0; 
-void setup()
-{
-  Serial.begin(9600); 
+void setup() {
+  Serial.begin(19200);
+  bluetoothSerial.begin(9600); // Start de Bluetooth-verbinding
 }
 
 void loop()
 {
-      motortemper = analogRead(motor);  
-      UTempmotor = motortemper/204.8;      
-      temperatuurmotor =  16+((UTempmotor - 1.52) / 0.0150); 
-      Serial.println("temperatuurmotor [C]: "); 
-      Serial.println(temperatuurmotor);   
-      baterijtemper = analogRead(motor);  
-      UTempbatterij = baterijtemper/204.8;     
-      temperatuurbaterij=  16+((UTempbatterij - 1.52) / 0.0150); 
-      Serial.println("temperatuurbaterij [C]: "); 
-      Serial.println(temperatuurbaterij);   
-      sensor3temper = analogRead(motor);  
-      UTempsensor3 = sensor3temper/204.8;     
-      temperatuursensor3 =  16+((UTempsensor3 - 1.52) / 0.0150);  
-      Serial.println("temperatuursensor3 [C]: "); 
-      Serial.println(temperatuursensor3);   
-      sensor4temper = analogRead(motor);  
-      UTempsensor4 = sensor4temper/204.8;      
-      temperatuursensor4 =  16+((UTempsensor4 - 1.52) / 0.0150);  
-      Serial.println("temperatuursensor4 [C]: "); 
-      Serial.println(temperatuursensor4);   
+  int motorreading = analogRead(motor);
+  float motorvoltage = motorreading * (5000 / 1024.0);
+  float motortemperature = (motorvoltage - 500) / 10;
+    Serial.println("temperatuurmotor [C]: "); 
+      Serial.println(motortemperature); 
+  
+  int batterijreading = analogRead(batterij);
+  float batterijvoltage = batterijreading * (5000 / 1024.0);
+  float batterijtemperature = (batterijvoltage - 500) / 10;
+    Serial.println("temperatuurmotor [C]: "); 
+      Serial.println(batterijtemperature);
+
+  int sensor3reading = analogRead(sensor3);
+  float sensor3voltage = sensor3reading * (5000 / 1024.0);
+  float sensor3temperature = (sensor3voltage - 500) / 10;
+    Serial.println("temperatuursensor3 [C]: "); 
+      Serial.println(sensor3temperature); 
+
+  int sensor4reading = analogRead(sensor4);
+  float sensor4voltage = sensor4reading * (5000 / 1024.0);
+  float sensor4temperature = (sensor4voltage - 500) / 10;
+    Serial.println("temperatuursensor3 [C]: "); 
+      Serial.println(sensor4temperature); 
+
 }
