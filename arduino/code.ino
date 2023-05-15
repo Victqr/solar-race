5babb760a8e5307528689187d022b893887256fb
Victor <68400813+Victqr@users.noreply.github.com>
2023-05-15 19:20:57 +0200

    Update code.ino

--- arduino/code.ino
+++ arduino/code.ino
@@ -76,19 +76,19 @@ void sensoren()
 {
   int motorreading = analogRead(motor);
   float motorvoltage = motorreading * (5000 / 1024.0);
-  float motortemperature = (motorvoltage - 500) / 10;
+  float motortemperature = (motorvoltage - 500) / 100;
     Serial.println("temperatuurmotor [C]: "); 
       Serial.println(motortemperature); 
   
   int batterijreading = analogRead(batterij);
   float batterijvoltage = batterijreading * (5000 / 1024.0);
-  float batterijtemperature = (batterijvoltage - 500) / 10;
+  float batterijtemperature = (batterijvoltage - 500) / 100;
     Serial.println("temperatuurmotor [C]: "); 
       Serial.println(batterijtemperature);
 
   int sensor3reading = analogRead(sensor3);
   float sensor3voltage = sensor3reading * (5000 / 1024.0);
-  float sensor3temperature = (sensor3voltage - 500) / 10;
+  float sensor3temperature = (sensor3voltage - 500) / 100;
     Serial.println("temperatuursensor3 [C]: "); 
       Serial.println(sensor3temperature); 
       
